1c07ab788b4a9a7b94c0221be44e2b359e6ad0ba
Rafael Andrade <projetos-andrade@outlook.com>
2021-05-25 01:02:07 -0300

    TAREFA 1 CONCLUÍDA

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from os import sep
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 import json
 import requests
@@ -6,17 +7,37 @@ import numpy as np
 
 views = Blueprint('views', __name__)
 
-class Clientes:
-    def __init__(self, nome, sobrenome, email, cep) -> None:
+# -- OBJETO CLIENTE --
+
+
+class Pessoa:
+    df_principal = pd.read_csv('data/clientes.csv', sep=';')
+    columns_name = []
+    for i in df_principal:
+        columns_name.append(i)
+
+    def __init__(self, nome, sobrenome, email, cep):
         self.nome = nome
         self.sobrenome = sobrenome
         self.email = email
         self.cep = cep
 
-## -- INFORMAÇÕES CLIENTES --
-lista_clientes = []
+    # TODO buscar informações de endereço da API do ViaCEP (https://viacep.com.br/)
 
-## -- PÁGINA INICIAL --
+    def busca(self):
+        response = requests.get(
+            f'http://www.viacep.com.br/ws/{self.cep}/json/')
+        # validando cep
+        if response.status_code == 200:
+            dados_json = json.loads(response.text)
+            df = pd.DataFrame(data=dados_json, index=[0])
+        else:
+            print('Cep inválido !')
+        return df
+
+# -- PÁGINA INICIAL --
+
+
 @views.route('/')
 def home():
     """
@@ -26,21 +47,12 @@ def home():
     return render_template('home.html')
 
 
-@views.route('/clientes')
-def clientes():
-    """
-    Rota para aba de clientes. Mostra na tela uma representação do csv de clientes
-    Não é necessário modificar nada nessa função
-    """
-    df = pd.read_csv('data/clientes.csv', dtype=object, sep=';')
-    df = df.replace(np.nan, '', regex=True)
-    return render_template('clientes.html', df=df, titles=df.columns.values, pessoas = lista_clientes)
+# -- CADASTRO --
 
-
-## -- CADASTRO --
 @views.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
+
 
 @views.route('/novo_cliente', methods=['POST', ])
 def novo_cliente():
@@ -48,33 +60,55 @@ def novo_cliente():
     Função para cadastro de novos clientes. Deverá pegar as informações do forms e salvar numa nova linha no csv.
     Necessário também salvar as informações de endereço provindas da API de CEP
     """
-    ## TODO pegar informações do forms
+    # TODO pegar informações do forms
+    p1 = request.form['nome']
+    p2 = request.form['sobrenome']
+    p3 = request.form['email']
+    p4 = request.form['cep']
 
-    nome = request.form['nome']
-    sobrenome = request.form['sobrenome']
-    email = request.form['email']
-    cep = request.form['cep']
+    pessoa1 = Pessoa(p1, p2, p3, p4)
 
-    informe = Clientes(nome, sobrenome, email, cep)
-    lista_clientes.append(informe)
+    dicio = {
+        'nome': pessoa1.nome,
+        'sobrenome': pessoa1.sobrenome,
+        'email': pessoa1.email,
+    }
+    df1 = pd.DataFrame(data=dicio, index=[0])
+
+    # TODO criar nova linha no arquivo csv
+
+    df2 = pessoa1.busca()
+    result = pd.concat([df1, df2], axis=1, ignore_index=True)
+    result.columns = Pessoa.columns_name
+
+    df_final = pd.concat([Pessoa.df_principal, result])
+
+    Pessoa.df_principal = df_final
 
     return redirect(url_for('views.clientes'))
- 
-    ## TODO buscar informações de endereço da API do ViaCEP (https://viacep.com.br/)
+
+# -- CONSULTA CEP --
 
 
-    ## TODO criar nova linha no arquivo csv
-
-## -- CONSULTA CEP --
 @views.route('/consulta-cep')
 def consulta_cep():
-    ## TODO pegar CEP do forms
+    # TODO pegar CEP do forms
 
+    # TODO buscar informações de endereço da API do ViaCEP (https://viacep.com.br/)
 
-    ## TODO buscar informações de endereço da API do ViaCEP (https://viacep.com.br/)
-
-
-    ## TODO mostrar no html as informações obtidas
-
+    # TODO mostrar no html as informações obtidas
 
     return render_template('consulta_cep.html')
+
+
+@views.route('/clientes')
+def clientes():
+    """
+    Rota para aba de clientes. Mostra na tela uma representação do csv de clientes
+    Não é necessário modificar nada nessa função
+    """
+    Pessoa.df_principal.to_csv('clientes.csv', index=False, sep=';')
+    
+    return render_template('clientes.html', 
+                            df=Pessoa.df_principal, 
+                            titles=Pessoa.df_principal.columns.values)
