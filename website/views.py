9cd8ba7167fd0a433a8fa28d595b27085a82494f
Rafael Andrade <projetos-andrade@outlook.com>
2021-05-25 01:03:13 -0300

    Update views.py

--- website/views.py
+++ website/views.py
@@ -94,10 +94,8 @@ def novo_cliente():
 def consulta_cep():
     # TODO pegar CEP do forms
 
-    # TODO buscar informações de endereço da API do ViaCEP (https://viacep.com.br/)
-
     # TODO mostrar no html as informações obtidas
-
+    
     return render_template('consulta_cep.html')
 
 
@@ -108,7 +106,7 @@ def clientes():
     Não é necessário modificar nada nessa função
     """
     Pessoa.df_principal.to_csv('clientes.csv', index=False, sep=';')
-    
+
     return render_template('clientes.html', 
                             df=Pessoa.df_principal, 
                             titles=Pessoa.df_principal.columns.values)
